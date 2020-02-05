15356633fd8b933b82abb543039707e1fd2d4dc9
TristanLee <tristan.rene.lee@gmail.com>
2020-02-04 20:30:11 -0800

    Prints if letter is part of the answer

--- Hangman.py
+++ Hangman.py
@@ -3,3 +3,16 @@ import random
 words = open("/Users/trist/Desktop/Coding/PythonProjects/Hangman/words.txt", "r")
 
 answer = random.choice(list(words))
+incorr_guess = 0
+
+cur_guesses = ["_"]*len(answer)
+
+while incorr_guess < 6:
+    guess = input("Guess a letter: ")
+    if guess in answer:
+        print("Correct! That is a letter in the word.")
+    else:
+        print("That is not a letter in the word")
+        incorr_guess += 1
+
+    print(*cur_guesses)
