eac93ab72e703ee0c4bc29646bd78798587de2d2
TristanLee <tristan.rene.lee@gmail.com>
2020-02-06 15:08:02 -0800

    Prints the current guesses in the correct location

--- Hangman.py
+++ Hangman.py
@@ -1,4 +1,5 @@
 import random 
+import re
 
 words = open("/Users/trist/Desktop/Coding/PythonProjects/Hangman/words.txt", "r")
 
@@ -7,16 +8,29 @@ incorr_guess = 0
 
 cur_guesses = ["_"]*len(answer)
 
+def printGuesses(guess, answer):
+    #count = 0
+    index = []
+
+    for i in range(len(answer)):
+        if answer[i] is guess:
+            cur_guesses[i] = guess 
+
+    return index
+            
+    
 
 while incorr_guess < 6:
     guess = input("Guess a letter: ")
     if guess in answer:
         print("Correct! That is a letter in the word.")
         #get index where the char is in the string
-        print(answer.find(guess))
-        
+        printGuesses(guess, answer)
+
     else:
         print("That is not a letter in the word")
         incorr_guess += 1
 
     print(*cur_guesses)
+
+print("\nThe correct answer is " + answer)
