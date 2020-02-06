ceeb5722d66e49c3ce406212d22136f758c3c2d9
TristanLee <tristan.rene.lee@gmail.com>
2020-02-06 14:17:05 -0800

    added comment

--- Hangman.py
+++ Hangman.py
@@ -7,10 +7,14 @@ incorr_guess = 0
 
 cur_guesses = ["_"]*len(answer)
 
+
 while incorr_guess < 6:
     guess = input("Guess a letter: ")
     if guess in answer:
         print("Correct! That is a letter in the word.")
+        #get index where the char is in the string
+        print(answer.find(guess))
+        
     else:
         print("That is not a letter in the word")
         incorr_guess += 1
