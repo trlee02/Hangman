ea488563fe60be1e03ac0517b20ab0cadf75ebee
TristanLee <tristan.rene.lee@gmail.com>
2020-02-06 19:46:41 -0800

    Cleaned output, can't guess same letter

--- Hangman.py
+++ Hangman.py
@@ -6,7 +6,7 @@ words = open("/Users/trist/Desktop/Coding/PythonProjects/Hangman/words.txt", "r"
 answer = random.choice(list(words))
 incorr_guess = 0
 
-cur_guesses = ["_"]*len(answer)
+cur_guesses = ["_"]*(len(answer)-1)
 
 def printGuesses(guess, answer):
     #count = 0
@@ -22,15 +22,18 @@ def printGuesses(guess, answer):
 
 while incorr_guess < 6:
     guess = input("Guess a letter: ")
-    if guess in answer:
-        print("Correct! That is a letter in the word.")
+    if guess in cur_guesses:
+        print("\nYou have already guessed \"{}\"".format(guess))
+    elif guess in answer:
+        print("\nCorrect! That is a letter in the word.\n")
         #get index where the char is in the string
         printGuesses(guess, answer)
 
     else:
-        print("That is not a letter in the word")
+        print("\nThat is not a letter in the word\n")
         incorr_guess += 1
 
     print(*cur_guesses)
+    print("")
 
 print("\nThe correct answer is " + answer)
