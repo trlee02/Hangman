806d999d67d77fd12d8bdb832df299380af92d6c
TristanLee <tristan.rene.lee@gmail.com>
2020-02-19 22:35:08 -0800

    Added replayable game

--- Hangman.py
+++ Hangman.py
@@ -32,25 +32,39 @@ def printMan():
         print(" O\n/|\\\n/")
     elif incorr_guess is 6:
         print(" O\n/|\\\n/ \\")
+
+running = False
+
+playing = input("Would you like to play Hangman? Y/N\n")
+if playing is "Y":
+    running = True
+else:
+    running = False
+
+while running:
+    while incorr_guess < 6:
+        guess = input("Guess a letter: ")
+        if guess in cur_guesses:
+            print("\nYou have already guessed \"{}\"".format(guess))
+        elif guess in answer:
+            print("\nCorrect! That is a letter in the word.\n")
+            #get index where the char is in the string
+            printGuesses(guess, answer)
+        else:
+            print("\nThat is not a letter in the word\n")
+            incorr_guess += 1
+
+        
+        print(*cur_guesses)
+        print("")
+        printMan()
     
+    play_again = input("Would you like to play again? Y/N")
+    if play_again is "N": 
+        running = False 
+    else: 
+        incorr_guess = 0
 
-while incorr_guess < 6:
-    guess = input("Guess a letter: ")
-    if guess in cur_guesses:
-        print("\nYou have already guessed \"{}\"".format(guess))
-    elif guess in answer:
-        print("\nCorrect! That is a letter in the word.\n")
-        #get index where the char is in the string
-        printGuesses(guess, answer)
-    else:
-        print("\nThat is not a letter in the word\n")
-        incorr_guess += 1
-
-    
-    print(*cur_guesses)
-    print("")
-    printMan()
-
-print("\nThe correct answer is " + answer)
+    print("\nThe correct answer is " + answer)
 
 
